6ba3519fefa4daf64f8c8a77a480d70894e5749b
poofytoo <victorhung92@gmail.com>
2014-12-04 13:05:00 -0500

    files

---
+++ js/scripts.js
@@ -0,0 +1,136 @@
+$(function() {
+var W = $(window).width();
+var H = $(window).height();
+
+$("#cover-circ")
+    .velocity({ cx: W/2,
+                cy: H/2,
+              }, {duration: 0})
+
+$("#circ")
+    .velocity({ cx: W/2,
+                cy: H/2,
+              }, {duration: 100})
+    .delay(100)
+    .velocity({ r: 100, 
+                strokeWidth: "30px",
+                opacity: 1 }, 
+              { duration: 700, easing: [.53,.01,.91,.69]}
+              );
+$("#arrow")
+    //.delay(500)
+    .velocity({ 
+                x: 0,
+                y: 0, 
+                translateX: W/2,
+                translateY: H/2,
+                rotateZ: 90,
+                //opacity: 0,
+                fill: '#121414'
+              }, {duration: 0})
+    .delay(1000)
+    
+    .velocity({ rotateZ: 180,
+                translateZ: 0,
+                fill: '#1bf8eb'
+                }, 
+              { duration: 1200, easing: [.6,.01,.42,1]}
+              );
+    /*
+$("#triangle")
+    .velocity({ translateX: W/2 - 100 - 87.7,
+                translateY: H/2,
+                scale: 0.5,
+                rotateZ: -45,
+                opacity: 0
+              }, 
+              { duration: 0})
+    .delay(1500)
+    .velocity({ translateX: [W/2 - 100 - 35, "ease-out"],
+                translateY: [H/2 - 100 - 35, "ease-out"],
+                rotateZ: [0, "ease-out"],
+                opacity: 1
+              }, 
+              { duration: 800, easing: [.53,.01,.91,.69]}
+              );*/
+/*
+
+var width = W+500,
+    height = H+500;
+
+var vertices = d3.range(100).map(function(d) {
+  return [Math.random() * width, Math.random() * height];
+});
+
+var svg = d3.select(".intro").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("class", "PiYG")
+
+var path = svg.append("g").selectAll("path");
+
+svg.selectAll("circle")
+    .data(vertices.slice(1))
+  .enter().append("circle")
+    .attr("transform", function(d) { return "translate(" + d + ")"; })
+    .attr("r", 2);
+
+redraw();
+
+function redraw() {
+  path = path.data(d3.geom.delaunay(vertices).map(function(d) { return "M" + d.join("L") + "Z"; }), String);
+  path.exit().remove();
+  path.enter().append("path").attr("class", function(d, i) { return "q" + (i % 9) + "-9"; }).attr("d", String);
+}
+*/
+var v1 = window.setInterval(function() {
+  $("#ring1").velocity({ 
+    r: 50,
+    opacity: 0.1}, 
+    {duration: 1000, easing: 'ease-out'})
+  $("#ring1").velocity({ 
+    r: 10,
+    opacity: 1}, 
+    {duration: 0})
+
+}, 1000);
+window.setTimeout(function() {
+    var v2 = window.setInterval(function() {
+    $("#ring2").velocity({ 
+      r: 50,
+      opacity: 0.1}, 
+      {duration: 1000, easing: 'ease-out'}) 
+    .velocity({ 
+      r: 10,
+      opacity: 1}, 
+    {duration: 0});
+    }, 1000)
+  }, 500)
+gyro.frequency = 100;
+  gyro.startTracking(function(o) {
+        // o.x, o.y, o.z for accelerometer
+        // o.alpha, o.beta, o.gamma for gyro
+        /*
+        $('body').css('color', '#fff');
+        */
+        angle = (o.alpha - 45)/180 * Math.PI;
+        x = Math.cos(angle)*50 + 150
+        y = Math.sin(angle)*50 + 90
+
+        $("#dw").velocity({ 
+          cx: x,
+          cy: y}, 
+          {duration: 0});
+
+        $('#dw-text').velocity({ 
+          x: x-14,
+          y: y+10}, 
+          {duration: 0});
+
+        $('#north-rect').velocity({ 
+          translateX: 150,
+          translateY: 90,
+          rotateZ: o.alpha-135 }, 
+          {duration: 0});
+    });
+})
